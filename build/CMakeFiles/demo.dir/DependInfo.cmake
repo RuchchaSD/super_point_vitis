64c501415b07d7321a56b1ea788aabb3cdcb8801
RuchchaSD <ruchithsd@gmail.com>
2024-09-25 14:56:21 +0000

    first commit

---
+++ build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/OurTest/super_point/src/demo_superpoint.cpp" "CMakeFiles/demo.dir/src/demo_superpoint.cpp.o" "gcc" "CMakeFiles/demo.dir/src/demo_superpoint.cpp.o.d"
+  "/home/ubuntu/OurTest/super_point/src/superpoint.cpp" "CMakeFiles/demo.dir/src/superpoint.cpp.o" "gcc" "CMakeFiles/demo.dir/src/superpoint.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ working/CMakeFiles/test_performance.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_performance.dir/src/superpoint.cpp.o"
+  "CMakeFiles/test_performance.dir/src/superpoint.cpp.o.d"
+  "CMakeFiles/test_performance.dir/src/test_performance_superpoint.cpp.o"
+  "CMakeFiles/test_performance.dir/src/test_performance_superpoint.cpp.o.d"
+  "test_performance"
+  "test_performance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_performance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
